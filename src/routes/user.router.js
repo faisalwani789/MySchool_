01ac56425659f6ac2a0b9e37724d024d6f02b539
Faisal yousuf wani <faisalyousuf1122@gmail.com>
2025-12-30 23:10:55 +0530

    added routes

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
-import { addUser ,getUsers} from "../controllers/user.controller.js";
+// import { addUser ,getUsers} from "../controllers/user.controller.js";
+import {addUser,getUsers} from "../controllers/user.controller2.js"
 import { UserSignIn } from "../controllers/auth.controller.js";
 const router=Router()
 router.get('/', getUsers)
