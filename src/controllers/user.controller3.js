a2a06fc9192fce04f5b8ccfb64d59e4e3b7c4863
Faisal yousuf wani <faisalyousuf1122@gmail.com>
2026-01-03 22:15:29 +0530

    added user Controller v3(getUsers)

--- src/controllers/user.controller3.js
+++ src/controllers/user.controller3.js
@@ -3,53 +3,98 @@ import { addTeacherWithClasses, assignRole, createParent, createUser, upsertStud
 
 
 export const addUsers = async (req, res) => {
-    const{roles,user,classes=[],children:rawChildren,parent,teacher}=req.body
-    
-    const children=Array.isArray(rawChildren) ? rawChildren : []
+    const { roles, user, classes = [], children: rawChildren, parent, teacher } = req.body
+
+    const children = Array.isArray(rawChildren) ? rawChildren : []
     try {
         await prisma.$transaction(async (tx) => {
-           
-            const mainUser =await createUser(tx,user)
-            let parentEntity=null
-            if(roles.isParent){
-                await assignRole(tx,mainUser.id,2)
-                parentEntity=await createParent(tx,mainUser,parent)
-            }
 
-            
-            if(roles.isTeacher){
-                 await assignRole(tx,mainUser.id,3)
-                const teacherEntity=await addTeacherWithClasses(tx,classes,mainUser,teacher)
-
+            const mainUser = await createUser(tx, user)
+            let parentEntity = null
+            if (roles.isParent) {
+                await assignRole(tx, mainUser.id, 2)
+                parentEntity = await createParent(tx, mainUser, parent)
             }
 
 
-            if (roles.isParent && children.length>0) {
+            if (roles.isTeacher) {
+                await assignRole(tx, mainUser.id, 3)
+                const teacherEntity = await addTeacherWithClasses(tx, classes, mainUser, teacher)
+
+            }
+
+
+            if (roles.isParent && children.length > 0) {
                 for (const child of children) {
-                    const childrenUser = await createUser(tx,child)
-                    await assignRole(tx,childrenUser.id,1)
+                    const childrenUser = await createUser(tx, child)
+                    await assignRole(tx, childrenUser.id, 1)
                     //adding student from student users
-                    const newStudent = await upsertStudent(tx,childrenUser,child,parentEntity.id)
+                    const newStudent = await upsertStudent(tx, childrenUser, child, parentEntity.id)
 
                 }
             }
 
 
         })
-    
-        res.status(201).json({success:true,message:'teacher child parent added successfully'})
+
+        res.status(201).json({ success: true, message: 'teacher child parent added successfully' })
     } catch (error) {
         console.log(error)
         res.status(500).send(error.message)
     }
 }
 
-export const getUsers=async(req,res)=>{
+const BaseQuery = {
+    select: {
+        first_name: true,
+        last_name: true,
+        email: true,
+        id: true,
+        userRole: true,
+        teacher: {
+            include: {
+                teaherClasses: {
+                    include: {
+                        teacherClassSubject: true
+                    }
+                }
+            }
+        },
+        student: {
+            include: {
+                class: {
+                    include: {
+                        classSubjects: {
+                            include: {
+                                subject: true
+                            }
+                        }
+                    }
+                }
+            }
+        },
+        parent: true
+    }
+}
+export const getUsers = async (req, res) => {
+    let users
     try {
-        await prisma.$transaction(async(tx)=>{
-            const users= await tx.user.findMany({
-                
+        await prisma.$transaction(async (tx) => {
+            if(req.body.id){
+                users=await tx.user.findUnique({
+                    where:{
+                        id:req.body.id
+                    },
+                    ...BaseQuery
+                })
+            }
+            else{
+                users = await tx.user.findMany({
+                    ...BaseQuery
             })
+            }
+             
+            res.json(users)
         })
     } catch (error) {
         res.status(500).send(error.message)
--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
 // import { addUser ,getUsers} from "../controllers/user.controller.js";
 import { authMiddleware,authRole } from "../middlewares/auth.middleware.js";
-import {getUsers} from "../controllers/user.controller2.js"
-import { addUsers } from "../controllers/user.controller3.js";
+// import {getUsers,getUsers} from "../controllers/user.controller2.js"
+import { addUsers,getUsers } from "../controllers/user.controller3.js";
 import { UserSignIn } from "../controllers/auth.controller.js";
 const router=Router()
 router.get('/',getUsers)
